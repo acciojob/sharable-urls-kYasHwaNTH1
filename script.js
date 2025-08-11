603fb2e2fd84e9f5290e191ca1c7e4a723e65702
Yashwanth Kesagoni <kesagoniyashwanth@gmail.com>
2025-08-11 18:31:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // your code here
+const url = document.getElementById("url");
+		const name = document.getElementById("name");
+		const year = document.getElementById("year");
+		const button = document.getElementById("button");
+		button.addEventListener("click",()=>{
+			if(name.value.trim()!=="" && year.value.trim()==""){
+				url.innerHTML = `https://localhost:8080/?name=${encodedURIComponent(name.value)}`
+			}
+			if(year.value.trim()!=="" && name.value.trim()==""){
+				url.innerHTML = `https://localhost:8080/?year=${encodedURIComponent(year.value)}`;
+			}
+			if(year.value.trim()!=="" && name.value.trim()!==""){
+				url.innerHTML = `https://localhost:8080/?name=${encodedURIComponent(name.value)}/?year=${encodedURIComponent(year.value)}`;
+			}
+		})
